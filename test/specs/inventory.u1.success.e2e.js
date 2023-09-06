4fc06613bc2d578b10b2a9926dc4a2cd32337990
Gary Wong <91444301+HKC0T@users.noreply.github.com>
2023-09-06 10:47:47 +0100

    Done login part, implementing multiple "it"

---
+++ test/specs/inventory.u1.success.e2e.js
@@ -0,0 +1,29 @@
+const { expect } = require("@wdio/globals");
+const LoginPage = require("../pageobjects/login.page");
+const InventoryPage = require("../pageobjects/inventory.page");
+describe("Login page", () => {
+  it("should login with accepted username 'standard_user' and redirect to inventory page", async () => {
+    await LoginPage.open();
+
+    await LoginPage.login("standard_user", "secret_sauce");
+
+    await expect(InventoryPage.filter).toBeExisting();
+    InventoryPage.selectFilter();
+
+    const item_prices = await $$("div[class='inventory_item_price']");
+  });
+  it("should login with accepted username 'standard_user' and redirect to inventory page", async () => {
+    await LoginPage.open();
+
+    await LoginPage.login("standard_user", "secret_sauce");
+
+    await expect(InventoryPage.filter).toBeExisting();
+    InventoryPage.selectFilter();
+
+    const item_prices = await $$("div[class='inventory_item_price']");
+  });
+});
+
+
+
+select sort items by price (low to high), items get sorrted correctly
